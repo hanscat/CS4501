0300cf80df83d4a3c1049c7e333dad252186433f
hanscat <hanscat@icloud.com>
2017-02-12 12:44:27 -0500

    demo.json written and models.py has primary keys added

--- models/api/models.py
+++ models/api/models.py
@@ -5,8 +5,9 @@ from django.db import models
 class user(models.Model):
     first_name = models.CharField(max_length=20)
     last_name  = models.CharField(max_length=20)
-    user_name = models.CharField(max_length=30)
-    password = models.CharField(max_length=30)
+    id = models.AutoField(primary_key=True)
+    user_name = models.AutoField(max_length=30)
+    password = models.AutoField(max_length=30)
     
     class Meta:
         abstract = True
@@ -19,6 +20,7 @@ class seller(user):
     car_sell = models.CharField(max_length=1)
 
 class car(models.Model):
+    VIN = models.AutoField(primary_key=True)
     car_color = models.CharField(max_length=30)
     car_brand = models.CharField(max_length=30)
     car_model = models.CharField(max_length=10)
@@ -35,9 +37,11 @@ class car_to_buy(car):
     price_to_offer = models.IntegerField(default=0)
 
 class inventory(models.Model):
+    id = models.AutoField(primary_key=True)
     owner = models.CharField(max_length=20)
     num = models.IntegerField(default=0)
     location = models.CharField(max_length = 20)
 
 class favorite(models.Model):
+    id = models.AutoField(primary_key=True)
     user: models.CharField(max_length=20)
