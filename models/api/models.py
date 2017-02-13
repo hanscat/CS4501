723024b3fc3c30c4270acc2f71ce34f0c05b8bc1
shadowgamefly <jerrysun1007@gmail.com>
2017-02-13 03:21:20 -0500

    finish class-based view for post/update/get

--- models/api/models.py
+++ models/api/models.py
@@ -7,7 +7,7 @@ class user(models.Model):
     last_name  = models.CharField(max_length=20)
     user_name = models.CharField(max_length=30)
     password = models.CharField(max_length=30)
-    
+
     class Meta:
         abstract = True
 
@@ -17,7 +17,7 @@ class car(models.Model):
     car_model = models.CharField(max_length=10)
     description = models.CharField(max_length=1000)
     price = models.IntegerField(default=0)
-    
+
     class Meta:
         abstract = True
 
@@ -28,15 +28,15 @@ class car_to_buy(car):
     price_to_offer = models.IntegerField(default=0)
 
 class buyer(user):
-    car_want = models.ManyToManyField(car_to_buy)
+    favourite = models.ManyToManyField(car_to_buy, blank = True)
 
 class seller(user):
-    car_sell = models.ManyToManyField(car_to_sell)
-    
+    car_sell = models.ManyToManyField(car_to_sell, blank = True)
+
 class inventory(models.Model):
     owner = models.CharField(max_length=20)
     num = models.IntegerField(default=0)
     location = models.CharField(max_length = 20)
 
-class favorite(models.Model):
-    user: models.CharField(max_length=20)
+# class favorite(models.Model):
+#     user = models.CharField(max_length=20)
--- models/api/views.py
+++ models/api/views.py
@@ -2,104 +2,212 @@ from django.shortcuts import render, get_object_or_404
 from django.http import JsonResponse, HttpResponse
 from django.core.exceptions import ObjectDoesNotExist
 from django.forms.models import model_to_dict
-from django.views.generic import ListView
+from django.views.generic import View, UpdateView, DeleteView
 from .models import *
 from .forms import *
+from django.db.models import Model
+from django.forms import ModelForm
 
 import json
 # Create your views here.
 
-def _success(data_dict, model_name, code):
-    correct = { 'status_code' : code, model_name : data_dict }
+def _success(data_dict, model_name):
+    correct = { model_name : data_dict }
     return JsonResponse(correct)
 
 def index(request):
     return HttpResponse("Welcome to API page.")
 
-class CarSellView(ListView):
-    model = car_to_sell
+class CarView(View):
+    model = Model
+    modelForm = ModelForm
 
     def get(self, request, *args, **kwargs):
-        car = get_object_or_404(car_to_sell, pk=kwargs['car_id'])
+        car = get_object_or_404(self.model, pk=kwargs['car_id'])
         car = model_to_dict(car)
-        return _success(car, 'car_to_sell', 200)
-
-    def post(self, request, *args, **kwargs):
-        def post(self, request, *args, **kwargs):
-            data = request.body.decode('utf-8')
-            data_dict = json.loads(data)
-            form = CarSellForm(data_dict)
-            if form.is_valid() :
-                form.save()
-                return HttpResponse('Success')
-            else :
-                return HttpResponse('Bad Post Request')
-
-
-class CarBuyView(ListView):
-    model = car_to_buy
-
-    def get(self, request, *args, **kwargs):
-        car= get_object_or_404(car_to_buy, pk=kwargs['car_id'])
-        car = model_to_dict(car)
-        return _success(car, 'car_to_buy', 200)
+        return _success(car, self.model.__name__)
 
     def post(self, request, *args, **kwargs):
         data = request.body.decode('utf-8')
         data_dict = json.loads(data)
-        form = CarBuyForm(data_dict)
+        form = self.modelForm(data_dict)
         if form.is_valid() :
+            if int(kwargs['car_id']) in self.model.objects.values_list('pk', flat = True) :
+                mes = self.update(kwargs['car_id'], data_dict)
+                return HttpResponse(mes)
             form.save()
-            return HttpResponse('Success')
+            return HttpResponse('Create Success')
         else :
             return HttpResponse('Bad Post Request')
 
-class BuyerView(ListView):
+    def update(self, car_id, data_dict):
+        car = get_object_or_404(self.model, pk = car_id)
+        form = self.modelForm(data_dict, instance = car)
+        form.save()
+        return "Success Update"
+
+class UserView(View):
+    model = Model
+    submodel = Model
+    modelForm = ModelForm
+
+    def get(self, request, *args, **kwargs):
+        user = get_object_or_404(self.model, pk=kwargs['user_id'])
+        user_want = model_to_dict(user)
+        l = []
+        if 'favourite' in [f.name for f in self.model._meta.get_fields()]:
+            for car in user_want['favourite'] :
+                l.append(car.pk)
+            user_want['favourite'] = l
+
+        elif "car_sell" in [f.name for f in self.model._meta.get_fields()]:
+            for car in user_want['car_sell'] :
+                l.append(car.pk)
+            user_want['car_sell'] = l
+
+        return _success(user_want, self.model.__name__)
+
+    def post(self, request, *args, **kwargs):
+        data = request.body.decode('utf-8')
+        data_dict = json.loads(data)
+        l = set()
+        if 'car_sell' in data_dict.keys():
+            for i in data_dict['car_sell']:
+                try :
+                    obj = self.submodel.objects.get(pk=i)
+                except ObjectDoesNotExist:
+                    return HttpResponse('No such car')
+                l.add(obj)
+            data_dict['car_sell'] = l
+
+        elif 'favourite' in data_dict.keys():
+            for i in data_dict['favourite']:
+                try :
+                    obj = self.submodel.objects.get(pk=i)
+                except ObjectDoesNotExist:
+                    return HttpResponse('No such car')
+                l.add(obj)
+            data_dict['favourite'] = l
+            # data_dict['car_sell'] = [self.submodel.objects.get(pk = i) for i in data_dict['car_sell']]
+        form = self.modelForm(data_dict)
+        if form.is_valid() :
+            if int(kwargs['user_id']) in self.model.objects.values_list('pk', flat = True) :
+                mes = self.update(kwargs['user_id'], data_dict)
+                return HttpResponse(mes)
+            form.save()
+            return HttpResponse('Create Success')
+        else :
+            return HttpResponse('Bad Post Request')
+
+    def update(self, user_id, data_dict):
+        car = get_object_or_404(self.model, pk = user_id)
+        form = self.modelForm(data_dict, instance = car)
+        form.save()
+        return "Success Update"
+
+
+class BuyerView(UserView):
     model = buyer
+    modelForm = BuyerForm
+    submodel = car_to_buy
 
-    def get(self, request, *args, **kwargs):
-        user= get_object_or_404(buyer, pk=kwargs['user_id'])
-        user_want = model_to_dict(user)
-        return _success(user_want, 'buyer', 200)
-
-    def post(self, request, *args, **kwargs):
-        data = request.body.decode('utf-8')
-        data_dict = json.loads(data)
-        form = BuyerForm(data_dict)
-        if form.is_valid() :
-            form.save()
-            return HttpResponse('Success')
-        else :
-            return HttpResponse('Bad Post Request')
-
-class SellerView(ListView):
+class SellerView(UserView):
     model = seller
+    modelForm = SellerForm
+    submodel = car_to_sell
 
-    def get(self, request, *args, **kwargs):
-        user= get_object_or_404(seller, pk=kwargs['user_id'])
-        user_want = model_to_dict(user)
-        return _success(user_want, 'seller', 200)
+class CarSellView(CarView):
+    model = car_to_sell
+    modelForm = CarSellForm
 
-    def post(self, request, *args, **kwargs):
-        data = request.body.decode('utf-8')
-        data_dict = json.loads(data)
-        form = SellerForm(data_dict)
-        if form.is_valid() :
-            form.save()
-            return HttpResponse('Success')
-        else :
-            return HttpResponse('Bad Post Request')
+class CarBuyView(CarView):
+    model = car_to_buy
+    modelForm = CarBuyForm
 
-    def update(request, user_id, *args, **kwargs):
-        user = get_object_or_404(seller, pk=kwargs['user_id'])
-        data = request.body.decode('utf-8')
-        data_dict = json.loads(data)
-        form = SellerForm(data_dict, instance = user)
-        if form.is_vaid() :
-            form.save()
-            return HttpResponse('Update Success')
-        else :
-            return HttpResponse('Bad Post request')
+# class CarSellView(View):
+#     model = car_to_sell
+#
+#     def get(self, request, *args, **kwargs):
+#         car = get_object_or_404(car_to_sell, pk=kwargs['car_id'])
+#         car = model_to_dict(car)
+#         return _success(car, 'car_to_sell')
+#
+#     def post(self, request, *args, **kwargs):
+#         data = request.body.decode('utf-8')
+#         data_dict = json.loads(data)
+#         form = CarSellForm(data_dict)
+#         if form.is_valid() :
+#             form.save()
+#             return HttpResponse('Success')
+#         else :
+#             return HttpResponse('Bad Post Request')
+#
+#
+# class CarBuyView(View):
+#     model = car_to_buy
+#
+#     def get(self, request, *args, **kwargs):
+#         car= get_object_or_404(car_to_buy, pk=kwargs['car_id'])
+#         car = model_to_dict(car)
+#         return _success(car, 'car_to_buy')
+#
+#     def post(self, request, *args, **kwargs):
+#         data = request.body.decode('utf-8')
+#         data_dict = json.loads(data)
+#         form = CarBuyForm(data_dict)
+#         if form.is_valid() :
+#             form.save()
+#             return HttpResponse('Success')
+#         else :
+#             return HttpResponse('Bad Post Request')
+
+
+# class BuyerView(View):
+#     model = buyer
+#
+#     def get(self, request, *args, **kwargs):
+#         user = get_object_or_404(buyer, pk=kwargs['user_id'])
+#         user_want = model_to_dict(user)
+#         l = []
+#         for car in user['favourite'] :
+#             l.append(car.pk)
+#         user['favourite'] = l;
+#         return _success(user_want, 'buyer')
+#
+#     def post(self, request, *args, **kwargs):
+#         data = request.body.decode('utf-8')
+#         data_dict = json.loads(data)
+#         form = BuyerForm(data_dict)
+#         if form.is_valid() :
+#             form.save()
+#             return HttpResponse('Success')
+#         else :
+#             return HttpResponse('Bad Post Request')
+#
+# class SellerView(View):
+#     model = seller
+#
+#     def get(self, request, *args, **kwargs):
+#         user = get_object_or_404(seller, pk=kwargs['user_id'])
+#         user = model_to_dict(user)
+#         l = []
+#         for car in user['car_sell'] :
+#             l.append(car.pk)
+#         user['car_sell'] = l;
+#         return _success(user, 'seller')
+#
+#     def post(self, request, *args, **kwargs):
+#         data = request.body.decode('utf-8')
+#         data_dict = json.loads(data)
+#         form = SellerForm(data_dict)
+#         if form.is_valid() :
+#             form.save()
+#             return HttpResponse('Success')
+#         else :
+#             return HttpResponse('Bad Post Request')
+
+
+
 
 # Original implementation changed to class_based view
 # def SellerDetail(request, user_id):
