65ce76263a1bd01d4669604432eada10764a359f
Hanscat <hanscat@icloud.com>
2017-02-24 00:39:57 -0500

    created views and urls for experience tier

--- exp/exp_service/views.py
+++ exp/exp_service/views.py
@@ -1,3 +1,34 @@
 from django.shortcuts import render
+from django.shortcuts import render
+import urllib.request, json
+from django.http import JsonResponse
+
 
 # Create your views here.
+modelsAPI = "http://localhost:8001/api/v1/"
+
+def invalidURL(request):
+    err = {}
+    err['message'] = "Oops, you just entered an invalid API request!"
+    err['status'] = False
+    return JsonResponse(err)
+
+#def individualUserData(request):
+
+def individualCarData(pk, request):
+    if request.method == 'GET':
+        urlForParticularCar = modelsAPI + "detail/car/"
+        requester = urllib.request.Request(urlForParticularCar + pk)
+        response = urllib.request.urlopen(requester).read().decode('utf-8')
+        car = json.loads(response)
+    return JsonResponse(car)
+
+
+def individualUserData(pk, request):
+    if request.method == 'GET':
+        urlForParticularCar = modelsAPI + "detail/user/"
+        requester = urllib.request.Request(urlForParticularCar + pk)
+        response = urllib.request.urlopen(requester).read().decode('utf-8')
+        user = json.loads(response)
+    return JsonResponse(user)
+
