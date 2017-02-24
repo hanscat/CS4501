a0181f4ec040a75075082557e2a38367e7049bff
shadowgamefly <jerrysun1007@gmail.com>
2017-02-23 23:35:33 -0500

    minor text update

--- models/models/views.py
+++ models/models/views.py
@@ -15,5 +15,5 @@ def internal_error(request):
     return _failure(500, "BOOM! I don't know what it going on now!")
 
 def _failure(code, message):
-    failure = {"Status Code" : code, "message" : message}
+    failure = {"status_code" : code, "message" : message}
     return JsonResponse(failure)
