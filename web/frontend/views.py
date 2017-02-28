a7dcd20ddbf871ee72b47e321de86958b76bddb2
Hanscat <hanscat@icloud.com>
2017-02-28 18:56:19 -0500

    demo pages added

--- web/frontend/views.py
+++ web/frontend/views.py
@@ -6,3 +6,11 @@ from django.template import loader
 def index(request):
     template = loader.get_template('home.html')
     return HttpResponse(template.render(request))
+
+def user_detail(request):
+    template = loader.get_template('userdetail.html')
+    return HttpResponse(template.render(request))
+
+def car_detail(request):
+    template = loader.get_template('cardetail.html')
+    return HttpResponse(template.render(request))
