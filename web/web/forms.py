eac909fb7da5f1d0fdb5a671b2cf786768048715
Ryan Bannon <rtb7rd@virginia.edu>
2017-04-03 20:34:13 -0400

    Login / createlisting front end

--- web/web/forms.py
+++ web/web/forms.py
@@ -1,8 +1,8 @@
 from django import forms
 
 class UserInfo(forms.Form):
-	username = forms.CharField(label='username', max_length=100)
-	password = forms.CharField(label='password', max_length=100)
+	username = forms.CharField(label='username', max_length=100, widget=forms.TextInput(attrs={'class': 'form-control'}))
+	password = forms.CharField(label='password', max_length=100, widget=forms.TextInput(attrs={'class': 'form-control', 'type': 'password'}))
 
 class SignupForm(forms.Form):
 	username = forms.CharField(label='Username', max_length=100)
@@ -23,11 +23,11 @@ Year_Choices=[(x, x) for x in range(1970, 2017)]
 TF=[(True, True), (False, False)]
 
 class listingForm(forms.Form):
-	car_color = forms.CharField(label='color')
-	car_make = forms.CharField(label='Car Make')
-	car_model = forms.CharField(label='Car Model')
+	car_color = forms.CharField(label='color', widget=forms.TextInput(attrs={'class': 'form-control'}))
+	car_make = forms.CharField(label='Car Make', widget=forms.TextInput(attrs={'class': 'form-control'}))
+	car_model = forms.CharField(label='Car Model', widget=forms.TextInput(attrs={'class': 'form-control'}))
 	car_year = forms.IntegerField(label='Year', widget=forms.Select(choices=Year_Choices))
-	car_body_type = forms.CharField(label='Body Type')
+	car_body_type = forms.CharField(label='Body Type', widget=forms.TextInput(attrs={'class': 'form-control'}))
 	car_new = forms.BooleanField(label='This is a new car', widget=forms.Select(choices=TF))
-	description = forms.CharField(label='Briefly Describe your car')
+	description = forms.CharField(label='Briefly Describe your car', widget=forms.Textarea)
 	price = forms.IntegerField(label='Price Listing')
