bef395cd7ceaba2e6f95c6a1d8b13f689f118f9d
Hanscat <hanscat@icloud.com>
2017-05-08 01:23:50 -0400

    finished pyspark script

--- coview/spark.py
+++ coview/spark.py
@@ -1,17 +1,40 @@
+import itertools
 from pyspark import SparkContext
 
 sc = SparkContext("spark://spark-master:7077", "PopularItems")
 
-data = sc.textFile("/tmp/data/access.log", 2)     # each worker loads a piece of the data file
+data = sc.textFile("/tmp/data/access.log", 2)  # each worker loads a piece of the data file
 
-pairs = data.map(lambda line: line.split("\t"))   # tell each worker to split each line of it's partition
-pages = pairs.map(lambda pair: (pair[1], 1))      # re-layout the data to ignore the user id
-count = pages.reduceByKey(lambda x,y: int(x)+int(y))        # shuffle the data so that each key is only on one worker
-                                                  # and then reduce all the values by adding them together
 
-output = count.collect()                          # bring the data back to the master node so we can print it out
-for page_id, count in output:
-    print ("page_id %s count %d" % (page_id, count))
-print ("Popular items done")
+# helper function to generate co-views
+def createUserCarTupleList(users):
+    tupleList = []
+    for i, j in itertools.combinations(sorted(users[1]), 2):
+        tupleList.append((users[0], (i, j)))
+    return tupleList
 
+
+# 1. Read in data user-car as K-V pairs from log file
+user = data.map(lambda line: line.split("\t"))  # tell each worker to split each line of it's partition
+
+# 2. Group data by user and car ids
+userList = user.groupByKey().map(lambda x: (x[0], set(x[1])))
+
+# 3. group the user's all co-views using the helper function
+userCarPairs = userList.flatMap(createUserCarTupleList(userList))
+
+# 4 only keep the cars in the tuples list
+itemPairUserList = userCarPairs.map(lambda pair: (pair[1], 1))
+itemPairUserList = itemPairUserList.distinct()
+
+# 5.enumerate the occurences
+itemPairCount = itemPairUserList.map(lambda x: (x[0], 1)).reduceByKey(lambda x, y: int(x) + int(y))
+
+# 6, Filter out unpopular cars, which are viewed by fewer than 3 people
+filteredCount = itemPairCount.filter(lambda x: x[1] >= 3)
+
+output = filteredCount.collect()  # bring the data back to the master node so we can print it out
+for tuple in output:
+    print(tuple)
+print("Popular items done")
 sc.stop()
